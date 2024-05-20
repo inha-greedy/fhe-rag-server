3a6a383a918e39ea74a1c8a27ee283f23bc071d6
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2024-05-20 20:36:41 +0900

    feat(session): replace user_id int to str(hash)

--- server/services/session.py
+++ server/services/session.py
@@ -2,21 +2,18 @@
 세션 설정에 사용되는 비즈니스 로직을 담은 코드 페이지입니다.
 """
 
+from typing import Dict
 from fastapi import Request
 
 
-USER_MAP = {"sequence": 1000}
+_user_map: Dict[str, str] = {"user_id": "qq"}
 
 
 def get_user_id() -> int:
     """
     get user_id
     """
-    try:
-        return USER_MAP["user_id"]
-
-    except KeyError:
-        return 999
+    return _user_map.get("user_id")
 
 
 def set_user_id(request: Request):
@@ -24,16 +21,5 @@ def set_user_id(request: Request):
     set user_id
     """
     origin = request.headers.get("origin")
-    agent = request.headers.get("user-agent")
-    user_key = f"origin:{origin}::agent:{agent}"
-    print(f"{user_key=}")
 
-    if user_key not in USER_MAP:
-        # sequence를 가져와 업데이트합니다
-        new_number = USER_MAP["sequence"] + 1
-        USER_MAP["sequence"] = new_number
-
-        # 가져온 sequence를 이용해 새 user를 등록합니다
-        USER_MAP[user_key] = new_number
-
-    USER_MAP["user_id"] = USER_MAP[user_key]
+    _user_map["user_id"] = origin
