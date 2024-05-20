5aeebaf8028006c56a27689da7f37104c3d6fb83
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2024-05-20 14:05:48 +0900

    feat(session): add session log

--- server/services/session.py
+++ server/services/session.py
@@ -5,7 +5,7 @@
 from fastapi import Request
 
 
-user_map = {"sequence": 1000}
+USER_MAP = {"sequence": 1000}
 
 
 def get_user_id() -> int:
@@ -13,7 +13,7 @@ def get_user_id() -> int:
     get user_id
     """
     try:
-        return user_map["user_id"]
+        return USER_MAP["user_id"]
 
     except KeyError:
         return 999
@@ -26,12 +26,14 @@ def set_user_id(request: Request):
     origin = request.headers.get("origin")
     agent = request.headers.get("user-agent")
     user_key = f"origin:{origin}::agent:{agent}"
+    print(f"{user_key=}")
 
-    if user_key not in user_map:
+    if user_key not in USER_MAP:
         # sequence를 가져와 업데이트합니다
-        user_map["sequence"] = user_map["sequence"] + 1
+        new_number = USER_MAP["sequence"] + 1
+        USER_MAP["sequence"] = new_number
 
         # 가져온 sequence를 이용해 새 user를 등록합니다
-        user_map[user_key] = user_map["sequence"]
+        USER_MAP[user_key] = new_number
 
-    user_map["user_id"] = user_map[user_key]
+    USER_MAP["user_id"] = USER_MAP[user_key]
